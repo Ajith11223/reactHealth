a4671fb1504d278d495eb0fd4b50d35c4d76ea4d
Fadlullaatgit <fadlullafadu007@gmail.com>
2023-09-27 17:20:58 +0530

    teste

---
+++ src/Components/SideBar/Datas.jsx
@@ -0,0 +1,60 @@
+import {RxDashboard} from 'react-icons/rx';
+
+export const datas = [
+    {
+        id:1,
+        icon:<RxDashboard/>,
+        text:"Dashboard",
+    },
+    {
+        id:2,
+        icon:<RxDashboard/>,
+        text:"Project Management",
+    },
+    {
+        id:3,
+        icon:<RxDashboard/>,
+        text:"Feasiblity Report",
+    },
+    {
+        id:4,
+        icon:<RxDashboard/>,
+        text:"Policy & Procedures",
+    },
+    {
+        id:5,
+        icon:<RxDashboard/>,
+        text:"Jobs",
+    },
+    {
+        id:6,
+        icon:<RxDashboard/>,
+        text:"Training",
+    },
+    {
+        id:7,
+        icon:<RxDashboard/>,
+        text:"Articles",
+    },
+    {
+        id:8,
+        icon:<RxDashboard/>,
+        text:"Users",
+    },
+    {
+        id:9,
+        icon:<RxDashboard/>,
+        text:"Facility Planning",
+    },
+    {
+        id:10,
+        icon:<RxDashboard/>,
+        text:"Post Jobs",
+    },
+    {
+        id:11,
+        icon:<RxDashboard/>,
+        text:"Post Jobs",
+    },
+
+]
---
+++ src/Components/SideBar/SideBarData.js
@@ -0,0 +1,35 @@
+import React ,{useState} from "react";
+import { datas } from "./Datas";
+import { Link } from "react-router-dom";
+
+const SidebarData = ({ toggle}) => {
+
+
+  return (
+    <div>
+      <div className="">
+        {datas.map((data) => {
+          return (
+            <Link
+              to={data.id === 2 ? "/projectmanagement" : "/"}
+              className={` flex items-center mt-2 p-3 cursor-pointer hover:bg-[#4A4BE4] hover:text-white rounded-xl rounded-l-none transition-all duration-300  left-4 bottom-4`}
+              key={data.id}
+            >
+              <div className="mr-8 text[1.7rem] text-brown">{data.icon}</div>
+              <div
+                className={`${
+                  toggle ? "opacity-0 delay-200" : ""
+                } text-[1rem] text-brown whitespace-pre`}
+              >
+                {data.text}
+              </div>
+            </Link>
+
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SidebarData;
