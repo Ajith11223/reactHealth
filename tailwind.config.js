c25d84e0d987c399113f0903d214b03ec1d9b318
Fadlullaatgit <fadlullafadu007@gmail.com>
2023-09-28 17:43:35 +0530

    login little changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
     extend: {
       colors:{
         primary:"#ff5733",
-      }
+      },
+      backgroundColor: {
+        'custom-blue': '#5051F9',
+      },
     },
   },
   plugins: [],
